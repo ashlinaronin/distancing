7599b909371d64a873a178791c46db2d572e6ddb
ashlinaronin <ashlinaronin@gmail.com>
2020-03-24 22:30:01 -0700

    attempt glow:

---
+++ Assets/AudioVisualization.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioVisualization : MonoBehaviour
+{
+    AudioSource audioSource;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        
+        //    Debug.Log(audioSource.time);
+        //    Debug.Log(audioSource.clip.length);
+        var percentCompleted = audioSource.time / audioSource.clip.length;
+        Debug.Log(percentCompleted * 100 + "%");
+    }
+}
