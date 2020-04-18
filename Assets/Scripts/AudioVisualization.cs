a0983bc7147341ed68f7304f1bdbfd70b2e5777e
ashlinaronin <ashlinaronin@gmail.com>
2020-04-18 14:33:02 -0700

    Refine visualization logic to ensure beacons aren't left in a state that appears to be less than fully listened when they should be

--- Assets/Scripts/AudioVisualization.cs
+++ Assets/Scripts/AudioVisualization.cs
@@ -15,8 +15,10 @@ public class AudioVisualization : MonoBehaviour
 
     bool visited = false;
 
-    public Vector4 startOffset = new Vector4(1f, 0.5f, 0f, 0f);
-    public Vector4 endOffset = new Vector4(1f, 0.5f, 0f, 0.5f);
+    private Vector4 startOffset = new Vector4(1f, 0.5f, 0f, 0f);
+
+    // slightly over 0.5 eliminates the seams on either pole of the capsule
+    private Vector4 endOffset = new Vector4(1f, 0.5f, 0f, 0.52f);
 
     [Serializable]
     public class TrackListenedEvent : UnityEvent<int,int>{};
@@ -33,21 +35,32 @@ public class AudioVisualization : MonoBehaviour
     {
         var isPlayerClose = CheckCloseToTag("MainCamera", 20);
 
+        // if player isn't close, we don't want to do anything else
+        if (!isPlayerClose) return;
+
         // initial visit to this beacon
-        if (isPlayerClose && !visited) {
+        if (!visited) {
             visited = true;
         }
 
         // subsequent frames while listening to this beacon
-        if (isPlayerClose && visited && playedSeconds <= audioSource.clip.length) {
+        if (visited && playedSeconds < audioSource.clip.length) {
             // add the number of seconds since the last frame
             playedSeconds += Time.deltaTime;
 
+            // account for rounding error -- if we are within 1s of fully played, make sure it is tracked as fully played
+            if ((float)Math.Ceiling(playedSeconds) == (float)Math.Ceiling(audioSource.clip.length))
+            {
+                playedSeconds = audioSource.clip.length;
+            }
+
+            // call event listened by game logic to keep track of global listened state
             secondListened.Invoke(trackNumber, (int)Math.Ceiling(playedSeconds));
             VisualizeListenedProgress();
         }
     }
 
+    // show progress by moving the UV offset for both children of this GO (minimap beacon and player scale beacon)
     private void VisualizeListenedProgress() {
         float t = playedSeconds / audioSource.clip.length;
 
